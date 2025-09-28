c25aeb7a982a2ca22a0ab4ef9ce28c91c8b2b11e
TriNT <trintse173254@fpt.edu.vn>
2025-09-28 19:58:36 +0700

    1stcommit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { Button } from "@/components/ui/button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button
-        className="bg-green-700 hover:bg-green-900 text-white"
-        size="lg"
-        variant="outline"
-      >
-        Click me
-      </Button>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
