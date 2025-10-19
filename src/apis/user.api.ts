b94b149de6cb3afb10a2195c59636f1afe29ab2e
TriNT <trintse173254@fpt.edu.vn>
2025-10-20 00:23:58 +0700

    fix userAPI

--- src/apis/user.api.ts
+++ src/apis/user.api.ts
@@ -1,11 +1,10 @@
 import { api } from "@/lib/axios/axios";
-import type { PaginationResponse } from "@/@types/common/pagination";
-import type { ItemBaseResponse } from "@/@types/response";
+import type { ItemBaseResponse, ListBaseResponse } from "@/@types/response";
 import type { UserFull } from "@/@types/auth.type";
 
 export const UserFullAPI = {
   getAll: (pageNumber = 1, pageSize = 10) =>
-    api.get<PaginationResponse<UserFull>>("/api/User", {
+    api.get<ListBaseResponse<UserFull>>("/api/User", {
       params: { pageNumber, pageSize },
     }),
   getById: async (id: string): Promise<UserFull> => {
